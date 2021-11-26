93bd0347c7f4ffc0a00001c69e6557379b81940a
tixooo <konstantin.stefanov87@gmail.com>
2021-11-26 10:52:47 +0200

    test2
    test2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDom from 'react-dom'
 
 function Greeting() {
-  return <h4>testing git</h4>
+  return <h4>testing git 2</h4>
 }
 
 ReactDom.render(<Greeting/>, document.getElementById('root'))
